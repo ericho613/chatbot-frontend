c8d7d4632f051637031f08711fa518f355a74e08
Eric Ho <ericho613@gmail.com>
2026-05-12 14:20:11 -0400

    Modified environment.prod.ts.  Added comments.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,12 @@
 export const environment = {
   production: true,
 
+  // The following should be used if you want the frontend and
+  // backend to run in the same Docker container
 
-//   If you later put nginx or a reverse proxy in front of both frontend and backend apps, this might become:
-apiBaseUrl: '/api'
+  //   If you later put nginx or a reverse proxy in front of both frontend and backend apps, this might become:
+  apiBaseUrl: '/api'
 
+  // apiBaseUrl: 'http://localhost:8000'
   // apiBaseUrl: 'http://your-ec2-public-hostname-or-domain:8000'
 };
